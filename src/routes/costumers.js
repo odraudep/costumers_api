6418051e6eef21b7b01d81ffc69965bc16ac2194
odraudep <72766650+odraudep@users.noreply.github.com>
2021-07-19 19:20:47 -0300

    add get costumers

--- src/routes/costumers.js
+++ src/routes/costumers.js
@@ -1,12 +1,17 @@
 import express from "express";
+import { readFile } from "fs/promises";
+import { resolve } from "path";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    method: req.method,
-    message: "Everything worked fine",
+router.get("/", async (req, res) => {
+  let data = await readFile(resolve(__dirname, "../data.json"), {
+    encoding: "utf-8",
   });
+
+  data = JSON.parse(data).costumers;
+
+  res.json({ data });
 });
 
 export default router;
