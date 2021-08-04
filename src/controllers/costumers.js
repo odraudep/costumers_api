97e87f0faadad34c8bf63dccfdc09ad5fc906ac9
odraudep <72766650+odraudep@users.noreply.github.com>
2021-08-04 20:08:58 -0300

    add update costumer

--- src/controllers/costumers.js
+++ src/controllers/costumers.js
@@ -44,3 +44,40 @@ export const addCostumer = async (req, res) => {
 
   res.json({ data: costumer });
 };
+
+export const updateCostumer = async (req, res) => {
+  const { id } = req.params;
+  const update = req.body;
+
+  let data = await readFile(resolve(__dirname, "../data.json"), {
+    encoding: "utf-8",
+  });
+
+  data = JSON.parse(data);
+
+  const costumer = data.costumers.find((cost) => cost.id == id);
+
+  if (!costumer) return res.json({ message: "Costumer not found" });
+
+  let err = 0;
+
+  for (const prop in update) {
+    if (!costumer[prop]) err++;
+    else costumer[prop] = update[prop];
+  }
+
+  if (!Object.keys(update).length) err++;
+
+  if (err > 0) return res.status(400).json({ message: "Invalid values" });
+
+  data.costumers = data.costumers.map((cost) =>
+    cost.id == id ? costumer : cost
+  );
+
+  await writeFile(
+    resolve(__dirname, "../data.json"),
+    JSON.stringify(data, null, 2)
+  );
+
+  res.json({ data: costumer });
+};
--- src/routes/costumers.js
+++ src/routes/costumers.js
@@ -1,10 +1,15 @@
 import express from "express";
 
-import { getCostumers, addCostumer } from "../controllers/costumers";
+import {
+  getCostumers,
+  addCostumer,
+  updateCostumer,
+} from "../controllers/costumers";
 
 const router = express.Router();
 
 router.get("/", getCostumers);
 router.post("/", addCostumer);
+router.put("/:id", updateCostumer);
 
 export default router;
